ee986b36411239aadac11a805bed934d80a0e338
Prashant Gaware <prashantgaware8@gmail.com>
2024-05-01 20:02:14 +0530

    LinkedList Operations

--- LinkedList/src/dsa/ll/basics/Test3.java
+++ LinkedList/src/dsa/ll/basics/Test3.java
@@ -1,5 +1,7 @@
 package dsa.ll.basics;
 
+import java.util.Iterator;
+
 // Implementation
 public class Test3 {
 	
@@ -33,6 +35,7 @@ public class Test3 {
 				System.out.print(temp.data + " ");
 				temp= temp.next;
 			}
+			System.out.println();
 		}
 		
 		int size() {
@@ -43,6 +46,29 @@ public class Test3 {
 				temp = temp.next;
 			}
 			return count;
+		}
+
+		void insertAtHead(int val) {
+			Node temp = new Node(val);
+			if(head == null) {
+//				head = tail = temp;
+				
+				insertAtEnd(val); 		// Recursive
+			} else {
+				temp.next = head;
+				head = temp;
+			}
+		}
+		
+		void inserAtIndex(int index, int val) {
+			Node t = new Node(val);
+			Node temp = head;
+			
+			for(int i=1; i<=index-1; i++) {
+				temp = temp.next;
+			}
+			t.next = temp.next;
+			temp.next = t;
 		}
 		
 	}
@@ -57,13 +83,23 @@ public class Test3 {
 		ll.insertAtEnd(3);
 		ll.insertAtEnd(4);
 		ll.insertAtEnd(4);
+		ll.inserAtIndex(3, 9);
+		ll.display();
 		
 		System.out.println(ll.size());
-		ll.display();
 		ll.insertAtEnd(5);
 		System.out.println();
 		System.out.println(ll.size());
 		ll.display();
+		ll.insertAtHead(1);
+		ll.insertAtHead(2);
+		ll.insertAtHead(3);
+		ll.insertAtHead(6);
+		ll.display();
 		
 	}
+
+
+
+	
 }
